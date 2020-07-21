c80e7a8469ff8e2b61c8584bf19fa8ceac6432dd
Zowie <zrnkubheka@gmail.com>
2020-07-22 00:58:13 +0200

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 /*jshint esversion: 6 */
 /*jshint esversion: 7 */
 
- 
 // Select container to append gridCell(s) later (as children).
 const container = document.getElementById('container');
 
@@ -13,12 +12,10 @@ const gridWidth = 600;
 let clearButton = document.getElementById('clearButton');
 clearButton.addEventListener('click', resetGrid);
 
-let gridSize = 32**2;
-
-
+let gridSize = 32 ** 2;
 
 // Generate div elements based on input and append to container.
-for (i = 0 ; i < gridSize ; i++) {
+for (i = 0; i < gridSize; i++) {
     let gridCell = document.createElement('div');
     gridCell.classList = 'gridCell';
 
@@ -26,26 +23,23 @@ for (i = 0 ; i < gridSize ; i++) {
 
     // Add Event Listener to trigger color change.
     gridCell.addEventListener('mouseover', colorChange);
-    
+
 }
 
-
 //Dynamic grid styling based on gridSize.
-container.style.gridTemplateColumns = 
-'repeat('+ Math.sqrt(gridSize) + ',' + gridWidth / Math.sqrt(gridSize) + 'px)';
+container.style.gridTemplateColumns =
+    'repeat(' + Math.sqrt(gridSize) + ',' + gridWidth / Math.sqrt(gridSize) + 'px)';
 
-container.style.gridTemplateRows = 
-'repeat(' + Math.sqrt(gridSize) +',' + gridHeight / Math.sqrt(gridSize) + 'px)';
-
-
+container.style.gridTemplateRows =
+    'repeat(' + Math.sqrt(gridSize) + ',' + gridHeight / Math.sqrt(gridSize) + 'px)';
 
 // Implements the color change when mouse is over target.
-function colorChange (e) {
-    event.target.style.backgroundColor = '#99CED3';   
+function colorChange(e) {
+    event.target.style.backgroundColor = '#99CED3';
 }
 
 // Draws new grid based on user input
-function resetGrid (e) {
+function resetGrid(e) {
 
     while (container.hasChildNodes()) {
         container.removeChild(container.firstChild);
@@ -53,25 +47,24 @@ function resetGrid (e) {
 
     let newGridSize = prompt('How many squares per side do you want? Enter a number (Maximum is 100).') ** 2;
 
-    if ( newGridSize <= 100 ** 2) {
+    if (newGridSize <= 100 ** 2) {
 
-        for (i = 0 ; i < newGridSize ; i++) {
-        let gridCell = document.createElement('div');
-        gridCell.classList = 'gridCell';
+        for (i = 0; i < newGridSize; i++) {
+            let gridCell = document.createElement('div');
+            gridCell.classList = 'gridCell';
 
-        container.appendChild(gridCell);
+            container.appendChild(gridCell);
 
-        // Add Event Listener to trigger color change.
-        gridCell.addEventListener('mouseover', colorChange);
+            // Add Event Listener to trigger color change.
+            gridCell.addEventListener('mouseover', colorChange);
 
-        container.style.gridTemplateColumns = 
-        'repeat('+ Math.sqrt(newGridSize) + ',' + gridWidth / Math.sqrt(newGridSize) + 'px)';
-        
-        container.style.gridTemplateRows = 
-        'repeat(' + Math.sqrt(newGridSize) +',' + gridHeight / Math.sqrt(newGridSize) + 'px)';
+            container.style.gridTemplateColumns =
+                'repeat(' + Math.sqrt(newGridSize) + ',' + gridWidth / Math.sqrt(newGridSize) + 'px)';
+
+            container.style.gridTemplateRows =
+                'repeat(' + Math.sqrt(newGridSize) + ',' + gridHeight / Math.sqrt(newGridSize) + 'px)';
         }
-    }
-    else {
+    } else {
         alert('This number is too large. Try something less than 100 squares per side.')
     }
 
